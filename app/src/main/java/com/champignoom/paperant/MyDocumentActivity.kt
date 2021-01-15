62855ae59ac1c01bd7ae936547d14cd8be2e783d
champignoom <champignoom@gmail.com>
2021-01-15 20:09:07 +0100

    caching finished

--- app/src/main/java/com/champignoom/paperant/MyDocumentActivity.kt
+++ app/src/main/java/com/champignoom/paperant/MyDocumentActivity.kt
@@ -2,8 +2,8 @@ package com.champignoom.paperant
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.renderscript.Allocation
 import android.renderscript.Element
@@ -11,37 +11,44 @@ import android.renderscript.RenderScript
 import android.renderscript.ScriptIntrinsicBlur
 import android.util.Log
 import android.view.MotionEvent
-import android.widget.Toast
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toDrawable
+import androidx.lifecycle.lifecycleScope
 import com.artifex.mupdf.fitz.Document
 import com.artifex.mupdf.fitz.android.AndroidDrawDevice
 import kotlinx.android.synthetic.main.activity_my_document.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 import java.io.ByteArrayOutputStream
-import kotlin.system.measureTimeMillis
-import kotlin.time.measureTime
+import java.util.concurrent.LinkedBlockingDeque
 
 class MyDocumentActivity : AppCompatActivity() {
     companion object {
-        val BLUR_RADIUS = 15f
+        const val BLUR_RADIUS = 15f
     }
 
     private var currentPosition = -1.0
     private var numPage = 0
     private var doc: Document? = null
     private var blurredCache: MutableMap<Int, ByteArray> = mutableMapOf()
-    private var bitmap: Bitmap? = null
+
+    private var pageQueue = LinkedBlockingDeque<Int>()
+    private var lastCacheTimestamp: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_document)
 
-        val uri = intent.data ?: run {return}
+        val uri = intent.data ?: return
         doc = Document.openDocument(uri.path)
         numPage = doc!!.countPages()
         Log.i("Paperant", "numPage = $numPage")
 
+        // wait for the image_view to be inflated with size, otherwise pdf not loaded
         pdf_image_view.viewTreeObserver.addOnGlobalLayoutListener {
             if (currentPosition < 0) {
-                renderPageIfDifferent(0)
+                renderPageIfDifferent(0.0)
                 currentPosition = 0.0
             }
         }
@@ -65,75 +72,116 @@ class MyDocumentActivity : AppCompatActivity() {
 
         round_pad.onDeltaListener = {
             // TODO: queue in another thread
-            val newPosition = (currentPosition + it).coerceIn(0.0, (numPage-1).toDouble())
-            renderPageIfDifferent(newPosition.toInt())
-            currentPosition = newPosition
+            renderPageIfDifferent((currentPosition + it).coerceIn(0.0, numPage-1.0))
         }
 
-        button_show_compressed.setOnClickListener {
-            val byteArray = blurredCache[currentPosition.toInt()]!!
-            var decompressedBitmap: Bitmap
-            val decompressTime = measureTimeMillis {
-                decompressedBitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-            }
-            pdf_image_view.setImageBitmap(decompressedBitmap)
-            Toast.makeText(this, "decompressTime=${decompressTime} ms, ${byteArray.size} / ${bitmap!!.width * bitmap!!.height} / ${bitmap!!.allocationByteCount}", Toast.LENGTH_LONG).show()
+        lifecycleScope.launch(newSingleThreadContext("backgroundRendererThread")) {
+            renderPagesInBackground()
         }
     }
 
-    private fun blurImage(bitmap: Bitmap) {
+    private fun setPageLoading(compressedCache: ByteArray?) {
+        progress_indicator.visibility = View.VISIBLE
+        if (compressedCache == null) {
+            pdf_image_view.setImageDrawable(null)
+        } else {
+            pdf_image_view.setImageBitmap(decompressBitmap(compressedCache))
+        }
+        Log.d("Paperant", "setPageLoading() changing pdf_image_view")
+        lastCacheTimestamp = System.currentTimeMillis()
+    }
+
+    private fun setPageLoaded(bitmap: Bitmap) {
+        progress_indicator.visibility = View.GONE
+        val durationFromCache = System.currentTimeMillis() - lastCacheTimestamp
+        if (pdf_image_view.drawable == null || durationFromCache < 200) {
+            pdf_image_view.setImageBitmap(bitmap)
+        } else {
+            val crossfader = TransitionDrawable(
+                arrayOf(
+                    pdf_image_view.drawable,
+                    BitmapDrawable(resources, bitmap)
+                )
+            )
+            pdf_image_view.setImageDrawable(crossfader)
+            crossfader.startTransition(100)
+        }
+        Log.d("Paperant", "setPageLoaded() changing pdf_image_view")
+    }
+
+    private fun renderPagesInBackground() {
+        while (true) {
+            val pageNum = pageQueue.takeLast()
+            if (pageNum in blurredCache && pageNum != currentPosition.toInt()) continue
+            Log.d("Paperant", "background renderer start rendering page ${pageNum}")
+
+            val bitmap = loadPage(pageNum)
+
+            runOnUiThread {
+                synchronized(currentPosition) {
+                    // TODO necessary?
+                    if (pageNum == currentPosition.toInt()) {
+                        setPageLoaded(bitmap)
+                    }
+                }
+            }
+
+            if (!blurredCache.contains(pageNum)) {
+                blurredCache[pageNum] = compressBitmap(blurredImage(bitmap))
+            }
+        }
+    }
+
+    private fun blurredImage(bitmap: Bitmap): Bitmap {
         val rs = RenderScript.create(this)
         val blurrer = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
         val input = Allocation.createFromBitmap(rs, bitmap)
-        val output = Allocation.createFromBitmap(rs, bitmap)
+        val output = Allocation.createTyped(rs, input.type)
         blurrer.setRadius(BLUR_RADIUS)
         blurrer.setInput(input)
         blurrer.forEach(output)
-        output.copyTo(bitmap)
+
+        // TODO: try 565
+        val outputBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        output.copyTo(outputBitmap)
         input.destroy()
         output.destroy()
         rs.destroy()
+
+        return outputBitmap
     }
+
+    private fun compressBitmap(bitmap: Bitmap): ByteArray {
+        val s = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.WEBP, 0, s)
+        return s.toByteArray()
+    }
+
+    private fun decompressBitmap(b: ByteArray) =
+        BitmapFactory.decodeByteArray(b, 0, b.size)
+
 
     private fun turnPage(delta: Int) {
-        val newPosition = (currentPosition.toInt() + delta).coerceIn(0, numPage-1)
+        val newPosition = (currentPosition + delta).coerceIn(0.0, numPage-1.0)
         renderPageIfDifferent(newPosition)
-        currentPosition = newPosition.toDouble()
     }
 
-    private fun renderPageIfDifferent(pageNum: Int) {
-        if (pageNum == currentPosition.toInt()) return
-
-        Log.i("Paperant", "turn page ${currentPosition.toInt()} -> $pageNum")
-
+    private fun loadPage(pageNum: Int): Bitmap {
+        Log.i("Paperant", "loadPage ${pageNum}")
         val page = doc!!.loadPage(pageNum)
         val ctm = AndroidDrawDevice.fitPage(page, pdf_image_view.width, pdf_image_view.height)
-        bitmap = AndroidDrawDevice.drawPage(page, ctm)
-        pdf_image_view.setImageBitmap(bitmap)
+        return AndroidDrawDevice.drawPage(page, ctm)
+    }
 
-        var compressionTime = 0L
-        var blurringTime = 0L
-        var copyTime = 0L
-        if (!(pageNum in blurredCache)) {
-//            val tmpBitmap = Bitmap.createBitmap(pdf_image_view.width, pdf_image_view.height, Bitmap.Config.ARGB_8888)
-            val tmpBitmap = bitmap!!.copy(bitmap!!.config, true)
+    private fun renderPageIfDifferent(newPosition: Double) {
+        synchronized(currentPosition) {
+            val pageNumChanged = (newPosition.toInt() != currentPosition.toInt())
+            currentPosition = newPosition
+            if (!pageNumChanged) return
 
-//            copyTime = measureTimeMillis {
-//                val canvas = Canvas(tmpBitmap)
-//                pdf_image_view.draw(canvas)
-//            }
-
-            blurringTime = measureTimeMillis {
-                blurImage(tmpBitmap)
-            }
-
-            compressionTime = measureTimeMillis {
-                val s = ByteArrayOutputStream()
-                tmpBitmap.compress(Bitmap.CompressFormat.WEBP, 0, s)
-                blurredCache[pageNum] = s.toByteArray()
-            }
+            val pageNum = currentPosition.toInt()
+            setPageLoading(blurredCache[pageNum])
+            pageQueue.addLast(pageNum)
         }
-
-        Toast.makeText(this, "copyTime = $copyTime ms, blurTime = $blurringTime ms, compressionTime = $compressionTime ms", Toast.LENGTH_SHORT).show()
     }
 }
